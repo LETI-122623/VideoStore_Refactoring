221e1e3127f28dd6960ece06adb7e9846276328f
LETI_122631 <adrls@iscte-iul.pt>
2025-10-31 18:47:33 +0000

    v9 "Finally we used Replace Conditional with Polymorphism to eliminate the switch statement. The getters of the amount and points became abstract in the Price class and concrete implementations were developed in the three subclasses. See how simple they are now!"

--- src/Ficha3/Main.java
+++ src/Ficha3/Main.java
@@ -1,22 +1,27 @@
 package Ficha3;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /***********************************************************
  * Filename: Main.java
- * @author fba 6 de Mai de 2013
+ * @author 122631, 122623 e 122638 31 de Outubro de 2025
  ***********************************************************/
 public abstract class Main
 {
 
 	/***********************************************************
 	 * @param args
+     * @throws IOException
 	 ***********************************************************/
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
-        Price regularPrice = new Price(Price.Code.REGULAR);
-        Price childrensPrice = new Price(Price.Code.CHILDRENS);
-        Price newReleasePrice = new Price(Price.Code.NEW_RELEASE);
+        Price regularPrice = new RegularPrice();
+        Price childrensPrice = new ChildrensPrice();
+        Price newReleasePrice = new NewReleasePrice();
 
-		Customer who = new Customer("Barack Obama");
+        Customer who = new Customer("Barack Obama");
         Movie m1 = new Movie("Life of Amalia", regularPrice);
         Movie m2 = new Movie("Peter Pan", childrensPrice);
         Movie m3 = new Movie("Donna del Lago", newReleasePrice);
@@ -26,6 +31,10 @@ public abstract class Main
 		who.addRental(new Rental(m3, 3));
 		
 		System.out.println(who.statement());
+
+        PrintWriter html = new PrintWriter(new FileWriter("webPages/statement.html"));
+        html.println(who.htmlStatement());
+        html.close();
 	}
 
 }
